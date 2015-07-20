fccd468c0dd6f0f1dba4aa3889aa3ae4dc573766
setphen <stwelin@gmail.com>
2015-07-20 16:26:30 -0400

    added laser weapon

---
+++ Assets/Scripts/Gameplay/Physics/laser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laser : MonoBehaviour {
+	internal float life;
+	// Use this for initialization
+	void Awake () {
+		life = 1.0f;
+		Debug.Log("I HERE");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		life -= 10f * Time.deltaTime;
+		if (life < 0){
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Trash"){
+			Destroy(other.gameObject);
+        }
+	}
+}
